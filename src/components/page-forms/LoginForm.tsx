5f195fa003fdcf4c0d797786547312cdd3fdc893
Angelo Ricardo <angeloengcomp@gmail.com>
2024-01-23 11:58:26 -0300

    Add handlePost function to handle form submission

--- src/components/page-forms/LoginForm.tsx
+++ src/components/page-forms/LoginForm.tsx
@@ -16,13 +16,22 @@ export function LoginForm() {
         formState: { errors, isSubmitting },
     } = useForm();
 
+    async function handlePost(data: any) {
+        try {
+            router.push('/dashboard/coordenador/projetos')
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
     return (
         <div>
-            <form action="">
+            <form onSubmit={handleSubmit(handlePost)}>
                 <InputText label={'E-mail'} name={'email'} register={register} errors={errors} />
                 <InputText type="password" label={'senha'} name={'password'} register={register} errors={errors} />
                 <button type="submit" className="btn btn-primary mt-3 w-full">Entrar</button>
-                <button type="button" onClick={()=>router.push("/auth/criar-conta")} className="btn btn-neutral my-3 w-full">Criar conta</button>
+                <button type="button" onClick={() => router.push("/auth/criar-conta")} className="btn btn-neutral my-3 w-full">Criar conta</button>
                 <Link href="/auth/recuperar-senha" className='text-primary hover:underline'>
                     Recuperar senha
                 </Link>
